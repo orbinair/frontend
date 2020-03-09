de93a6135b908f1702a39f0b8ee292ad4338a53b
Kirill Sedunov <kirill.sedunov@outlook.com>
2020-03-09 18:43:09 +0300

    Added List and ListItem components
    - Added List component
    - Added ListItem component
    - Added basic List component documentation
    - Added basic ListItem component documentation

---
+++ src/components/List/List.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+const List = ({ children }) => (
+  <div>
+    {children}
+  </div>
+)
+
+List.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default List
---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+const ListItem = ({
+  children,
+  control,
+  className
+}) => (
+  <div
+    className={className}
+  >
+    {children}
+    <div>
+      {control}
+    </div>
+  </div>
+)
+
+ListItem.propTypes = {
+  children: PropTypes.node.isRequired,
+  control: PropTypes.node,
+  className: PropTypes.string,
+}
+
+export default ListItem
