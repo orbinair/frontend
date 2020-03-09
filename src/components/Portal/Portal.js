cd5170146dd4c80af6aabb8f3db18d6529763257
Kirill Sedunov <kirill.sedunov@outlook.com>
2020-03-09 19:32:19 +0300

    Added Portal component

---
+++ src/components/Portal/Portal.js
@@ -0,0 +1,10 @@
+﻿import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+const Portal = ({ children }) => ReactDOM.createPortal(children, document.body)
+
+Portal.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Portal
